1266e84c44d4a268f775727f2d1937ac66621a2e
oyyasumi177 <o.i.ivchenko@student.khai.edu>
2025-09-19 11:32:09 +0300

    повідомлення о виправленні

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,10 @@
 public class Test {
     public static void main(String[] args) {
+<<<<<<< HEAD
         System.out.println("Java education");
+=======
+        System.out.println("education");
+        //comment
+>>>>>>> d61d25e (added a comment to Test.java on the featire branch)
     }
 }

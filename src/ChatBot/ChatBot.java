eabb5c0001a89a998537cd923a89e7bd085bdcc9
oyyasumi177 <o.i.ivchenko@student.khai.edu>
2025-10-03 10:08:54 +0300

    ChatBot 1st stage

---
+++ src/ChatBot/ChatBot.java
@@ -0,0 +1,8 @@
+package ChatBot;
+
+public class ChatBot {
+    public static void main(String[] args) {
+        System.out.println("Hello! My name is IvchenkoBot");
+        System.out.println("I was created in 2025");
+    }
+}

0f311937cd392fefa26bcb33239ed5746a1d0942
oyyasumi177 <o.i.ivchenko@student.khai.edu>
2025-10-03 10:20:07 +0300

    ChatBot 2nd stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,8 +1,14 @@
 package ChatBot;
 
+import java.util.Scanner;
+
 public class ChatBot {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         System.out.println("Hello! My name is IvchenkoBot");
         System.out.println("I was created in 2025");
+        System.out.println("Please, remind me your name");
+        String Mynameis = input.nextLine();
+        System.out.println("What a great name you have," + Mynameis + "!");
     }
 }

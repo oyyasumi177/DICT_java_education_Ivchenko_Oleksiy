68cafe1ce4fc1924e39fee85a054c7d31b9b3d87
oyyasumi177 <o.i.ivchenko@student.khai.edu>
2025-10-12 13:04:48 +0300

    Hangman stage 2 complete

--- src/hangman.java
+++ src/hangman.java
@@ -1,8 +1,21 @@
+import javax.crypto.SecretKey;
+import java.util.Scanner;
+
 public class hangman {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         // Stage 1
-        System.out.println("Hangman");
+        System.out.println("HANGMAN");
         System.out.println("The game will be able soon");
         // Stage 2
+        System.out.println("HANGMAN");
+        System.out.print("Guess the word: > ");
+        String stage2 = input.nextLine();
+        String secret = "java";
+        if (stage2.equals("java")){
+            System.out.println("You survived!");
+        } else {
+            System.out.println("You lost!");
+        }
     }
 }

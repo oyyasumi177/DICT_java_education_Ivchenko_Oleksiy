02ad90051cf0c6296c44f25c2c4d87f8930c2ad6
oyyasumi177 <o.i.ivchenko@student.khai.edu>
2025-10-12 13:40:39 +0300

    Hangman stage 4 complete

--- src/hangman.java
+++ src/hangman.java
@@ -6,28 +6,18 @@ public class hangman {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Random rand = new Random();
-        // Stage 1
-        System.out.println("HANGMAN");
-        System.out.println("The game will be able soon");
-        // Stage 2
-        System.out.println("HANGMAN");
-        System.out.print("Guess the word: > ");
-        String stage2 = input.nextLine();
-        String secret = "java";
-        if (stage2.equals("java")){
-            System.out.println("You survived!");
-        } else {
-            System.out.println("You lost!");
-        }
-        // Stage 3
+        // Stage 4
         System.out.println("HANGMAN");
         String[] w = {"python", "java", "javascript", "kotlin"};
-
         int index = rand.nextInt(w.length);
         String secret2 = w[index];
-        System.out.print("Guess the word: > ");
+        String stage4 = secret2.substring(0,2);
+        for (int i = 2; i < secret2.length(); i++){
+            stage4 = stage4 + "-";
+        }
+        System.out.print("Guess the word " + stage4 + ": > ");
         String stage3 = input.nextLine();
-        if (stage2.equals(secret2)){
+        if (stage3.equals(secret2)){
             System.out.println("You survived!");
         } else {
             System.out.println("You lost!");

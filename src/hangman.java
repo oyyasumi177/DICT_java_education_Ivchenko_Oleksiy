64d17772d8f234d54d53a526ec7600973fb5f442
oyyasumi177 <o.i.ivchenko@student.khai.edu>
2025-10-12 12:52:28 +0300

    Hangman stage 1 complete

---
+++ src/hangman.java
@@ -0,0 +1,8 @@
+public class hangman {
+    public static void main(String[] args) {
+        // Stage 1
+        System.out.println("Hangman");
+        System.out.println("The game will be able soon");
+        // Stage 2
+    }
+}

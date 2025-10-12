df4143328607ca16f3c8818e2b2158cab1864497
oyyasumi177 <o.i.ivchenko@student.khai.edu>
2025-10-12 13:19:10 +0300

    Hangman stage 3 complete

--- src/hangman.java
+++ src/hangman.java
@@ -1,9 +1,11 @@
 import javax.crypto.SecretKey;
+import java.util.Random;
 import java.util.Scanner;
 
 public class hangman {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        Random rand = new Random();
         // Stage 1
         System.out.println("HANGMAN");
         System.out.println("The game will be able soon");
@@ -13,6 +15,19 @@ public class hangman {
         String stage2 = input.nextLine();
         String secret = "java";
         if (stage2.equals("java")){
+            System.out.println("You survived!");
+        } else {
+            System.out.println("You lost!");
+        }
+        // Stage 3
+        System.out.println("HANGMAN");
+        String[] w = {"python", "java", "javascript", "kotlin"};
+
+        int index = rand.nextInt(w.length);
+        String secret2 = w[index];
+        System.out.print("Guess the word: > ");
+        String stage3 = input.nextLine();
+        if (stage2.equals(secret2)){
             System.out.println("You survived!");
         } else {
             System.out.println("You lost!");

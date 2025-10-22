ecd99489c8c6f1267e756151422f014e525a8f19
oyyasumi177 <o.i.ivchenko@student.khai.edu>
2025-10-22 20:49:28 +0300

    CoffeeMachine stage 3 complete

--- src/CoffeeMachine/CoffeeMachine.java
+++ src/CoffeeMachine/CoffeeMachine.java
@@ -3,37 +3,42 @@ package CoffeeMachine;
 import java.util.Scanner;
 
 class Cup {
-    private static final int water = 200;
-    private static final int milk = 50;
-    private static final int beans = 15;
+    private static final int waterPerCup = 200;
+    private static final int milkPerCup = 50;
+    private static final int beansPerCup = 15;
 
-    public static void calculateIngredients(int cups) {
-        int waterTotal = water * cups;
-        int milkTotal = milk * cups;
-        int beansTotal = beans * cups;
-
-        System.out.println("For " + cups + " cups of coffee you will need:");
-        System.out.println(waterTotal + " ml of water");
-        System.out.println(milkTotal + " ml of milk");
-        System.out.println(beansTotal + " g of coffee beans");
+    public static int calculateIngredients(int water, int milk, int beans) {
+        int Watercups = water / waterPerCup;
+        int Milkcups = milk / milkPerCup;
+        int Beanscups = beans / beansPerCup;
+        return Math.min(Watercups, Math.min(Milkcups, Beanscups));
     }
 }
-
 public class CoffeeMachine {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Write how many cups of coffee you will need:");
-        int cups = input.nextInt();
+        System.out.println("Write how many ml of water the coffee machine has:");
+        int water = input.nextInt();
 
-        Cup.calculateIngredients(cups);
-        
-        System.out.println("Starting to make a coffee");
-        System.out.println("Grinding coffee beans");
-        System.out.println("Boiling water");
-        System.out.println("Mixing boiled water with crushed coffee beans");
-        System.out.println("Pouring coffee into the cup");
-        System.out.println("Pouring some milk into the cup");
-        System.out.println("Coffee is ready!");
+        System.out.println("Write how many ml of milk the coffee machine has:");
+        int milk = input.nextInt();
+
+        System.out.println("Write how many grams of coffee beans the coffee machine has:");
+        int beans = input.nextInt();
+
+        System.out.println("Write how many cups of coffee you will need:");
+        int Cupsneeded = input.nextInt();
+
+        int possibleCups = Cup.calculateIngredients(water, milk, beans);
+        if (possibleCups == Cupsneeded) {
+            System.out.println("Yes, I can make that amount of coffee");
+        } else if (possibleCups > Cupsneeded) {
+            int extra = possibleCups - Cupsneeded;
+            System.out.println("Yes, I can make that amount of coffee (and even " + extra + " more than that)");
+        } else {
+            System.out.println("No, I can make only " + possibleCups + " cups of coffee");
+        }
+
     }
 }
 

cd7baad8bbcbdc532ae504e499058fe7cff46332
oyyasumi177 <o.i.ivchenko@student.khai.edu>
2025-10-28 14:03:37 +0200

    TicTacToe stage 2 complete

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -1,9 +1,29 @@
 package TicTacToe;
 
+import java.util.Scanner;
+
 public class TicTacToe {
     public  static void main(String[] args) {
-        System.out.println("X O X");
-        System.out.println("O X O");
-        System.out.println("X X O");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter cells: ");
+        String cells = input.nextLine();
+
+        if (cells.length() != 9){
+            System.out.println("Invalid input");
+            return;
+        }
+        for (int i = 0; i < cells.length(); i++) {
+            char c = cells.charAt(i);
+            if (c != 'X' && c != 'O' && c != '_') {
+                System.out.println("Error:");
+                return;
+            }
+        }
+
+        System.out.println("---------");
+        System.out.println("| " + cells.charAt(0) + " " + cells.charAt(1) + " " + cells.charAt(2) + " |");
+        System.out.println("| " + cells.charAt(3) + " " + cells.charAt(4) + " " + cells.charAt(5) + " |");
+        System.out.println("| " + cells.charAt(6) + " " + cells.charAt(7) + " " + cells.charAt(8) + " |");
+        System.out.println("---------");
     }
 }

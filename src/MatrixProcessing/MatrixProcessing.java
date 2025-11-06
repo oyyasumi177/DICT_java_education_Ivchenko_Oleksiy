bb01a72a82d045a246adf730c146704551e8a10d
oyyasumi177 <o.i.ivchenko@student.khai.edu>
2025-11-06 14:05:47 +0200

    MatrixProcessing stage 2 complete

--- src/MatrixProcessing/MatrixProcessing.java
+++ src/MatrixProcessing/MatrixProcessing.java
@@ -8,32 +8,15 @@ public class MatrixProcessing {
         int n1 = sc.nextInt();
         int m1 = sc.nextInt();
         int[][] matrix = new int[n1][m1];
-        for(int i=0;i<n1;i++){
-            for(int j=0;j<m1;j++){
+        for(int i=0;i<n1;i++) {
+            for (int j = 0; j < m1; j++) {
                 matrix[i][j] = sc.nextInt();
             }
         }
-        int n2 = sc.nextInt();
-        int m2 = sc.nextInt();
-        int[][] matrix2 = new int[n2][m2];
-        for(int i=0;i<n2;i++){
-            for(int j=0;j<m2;j++){
-                matrix2[i][j] = sc.nextInt();
-            }
-        }
-        if (n1 != n2 || m1 != m2) {
-            System.out.println("ERROR");
-            return; // завершуємо програму
-        }
-        int[][] matrix3 = new int[n1][m1];
-        for(int i=0;i<n2;i++){
-            for(int j=0;j<m2;j++){
-                matrix3[i][j] = matrix[i][j] +  matrix2[i][j];
-            }
-        }
+        int konst = sc.nextInt();
         for (int i = 0; i < n1; i++) {
             for (int j = 0; j < m1; j++) {
-                System.out.print(matrix3[i][j]);
+                System.out.print(matrix[i][j]*konst);
                 if (j < m1 - 1) System.out.print(" ");
             }
             System.out.println();

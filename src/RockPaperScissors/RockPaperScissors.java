d476e0cb281253bd6e3abb3cf49b641ff33f722e
oyyasumi177 <o.i.ivchenko@student.khai.edu>
2025-12-07 17:52:27 +0200

    RockPaperScissors stage finale complete

--- src/RockPaperScissors/RockPaperScissors.java
+++ src/RockPaperScissors/RockPaperScissors.java
@@ -4,7 +4,34 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
 
+class GameLogic {
+
+    private List<String> options;
+
+    GameLogic(List<String> options) {
+        this.options = options;
+    }
+
+    String getResult(String user, String comp) {
+        if (user.equals(comp)) {
+            return "draw";
+        }
+
+        int index = options.indexOf(user);
+
+        List<String> rotated = new ArrayList<>();
+        rotated.addAll(options.subList(index + 1, options.size()));
+        rotated.addAll(options.subList(0, index));
+
+        int half = rotated.size() / 2;
+        List<String> beatsUser = rotated.subList(0, half);
+
+        return beatsUser.contains(comp) ? "lose" : "win";
+    }
+}
+
 public class RockPaperScissors {
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
@@ -14,27 +41,23 @@ public class RockPaperScissors {
         String name = scanner.nextLine();
         System.out.println("Hello, " + name);
 
-        int rating = 0;
+        int rating = loadRating(name);
 
-        try {
-            File file = new File("rating.txt");
-            Scanner fileScanner = new Scanner(file);
+        String inputOptions = scanner.nextLine().trim();
 
-            while (fileScanner.hasNext()) {
-                String userName = fileScanner.next();
-                int userScore = fileScanner.nextInt();
+        List<String> options = new ArrayList<>();
 
-                if (userName.equals(name)) {
-                    rating = userScore;
-                    break;
-                }
+        if (inputOptions.isEmpty()) {
+            options = Arrays.asList("rock", "paper", "scissors");
+        } else {
+            for (String s : inputOptions.split(",")) {
+                options.add(s.trim());
             }
-
-            fileScanner.close();
-        } catch (FileNotFoundException e) {
         }
 
-        String[] options = {"rock", "paper", "scissors"};
+        GameLogic logic = new GameLogic(options);
+
+        System.out.println("Okay, let's start");
 
         while (true) {
 
@@ -50,28 +73,51 @@ public class RockPaperScissors {
                 continue;
             }
 
-            if (!user.equals("rock") && !user.equals("paper") && !user.equals("scissors")) {
+            if (!options.contains(user)) {
                 System.out.println("Invalid input");
                 continue;
             }
 
-            String comp = options[random.nextInt(3)];
+            String comp = options.get(random.nextInt(options.size()));
+            String result = logic.getResult(user, comp);
 
-            if (user.equals(comp)) {
-                System.out.println("There is a draw (" + comp + ")");
-                rating += 50;
-            }
-            else if (
-                    (user.equals("rock") && comp.equals("scissors")) ||
-                            (user.equals("scissors") && comp.equals("paper")) ||
-                            (user.equals("paper") && comp.equals("rock"))
-            ) {
-                System.out.println("Well done. The computer chose " + comp + " and failed");
-                rating += 100;
-            }
-            else {
-                System.out.println("Sorry, but the computer chose " + comp);
+            switch (result) {
+                case "draw":
+                    System.out.println("There is a draw (" + comp + ")");
+                    rating += 50;
+                    break;
+
+                case "win":
+                    System.out.println("Well done. The computer chose " + comp + " and failed");
+                    rating += 100;
+                    break;
+
+                case "lose":
+                    System.out.println("Sorry, but the computer chose " + comp);
+                    break;
             }
         }
     }
+
+    private static int loadRating(String name) {
+        int rating = 0;
+
+        try {
+            File file = new File("rating.txt");
+            Scanner f = new Scanner(file);
+
+            while (f.hasNext()) {
+                String user = f.next();
+                int score = f.nextInt();
+                if (user.equals(name)) {
+                    rating = score;
+                }
+            }
+
+            f.close();
+        } catch (FileNotFoundException ignored) {}
+
+        return rating;
+    }
 }
+

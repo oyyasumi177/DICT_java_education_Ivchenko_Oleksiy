b20e9216cfcea9eb616daccf4acdb35fa0096cad
oyyasumi177 <o.i.ivchenko@student.khai.edu>
2025-12-07 13:34:40 +0200

    RPS stage 4 complete

--- src/RockPaperScissors/RockPaperScissors.java
+++ src/RockPaperScissors/RockPaperScissors.java
@@ -1,13 +1,38 @@
 package RockPaperScissors;
 
-import java.util.Random;
-import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
 
 public class RockPaperScissors {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
+
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name);
+
+        int rating = 0;
+
+        try {
+            File file = new File("rating.txt");
+            Scanner fileScanner = new Scanner(file);
+
+            while (fileScanner.hasNext()) {
+                String userName = fileScanner.next();
+                int userScore = fileScanner.nextInt();
+
+                if (userName.equals(name)) {
+                    rating = userScore;
+                    break;
+                }
+            }
+
+            fileScanner.close();
+        } catch (FileNotFoundException e) {
+        }
 
         String[] options = {"rock", "paper", "scissors"};
 
@@ -20,6 +45,11 @@ public class RockPaperScissors {
                 break;
             }
 
+            if (user.equals("!rating")) {
+                System.out.println("Your rating: " + rating);
+                continue;
+            }
+
             if (!user.equals("rock") && !user.equals("paper") && !user.equals("scissors")) {
                 System.out.println("Invalid input");
                 continue;
@@ -29,6 +59,7 @@ public class RockPaperScissors {
 
             if (user.equals(comp)) {
                 System.out.println("There is a draw (" + comp + ")");
+                rating += 50;
             }
             else if (
                     (user.equals("rock") && comp.equals("scissors")) ||
@@ -36,6 +67,7 @@ public class RockPaperScissors {
                             (user.equals("paper") && comp.equals("rock"))
             ) {
                 System.out.println("Well done. The computer chose " + comp + " and failed");
+                rating += 100;
             }
             else {
                 System.out.println("Sorry, but the computer chose " + comp);

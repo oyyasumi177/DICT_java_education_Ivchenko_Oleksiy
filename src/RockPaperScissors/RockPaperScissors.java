6860d8ff7c83c8e9181246ccd4baa4f89b2f6407
oyyasumi177 <o.i.ivchenko@student.khai.edu>
2025-12-07 13:13:11 +0200

    RPS stage 3 complete

--- src/RockPaperScissors/RockPaperScissors.java
+++ src/RockPaperScissors/RockPaperScissors.java
@@ -7,23 +7,39 @@ public class RockPaperScissors {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        String user = scanner.nextLine();
+        Random random = new Random();
 
         String[] options = {"rock", "paper", "scissors"};
 
-        Random random = new Random();
-        String comp = options[random.nextInt(3)];
+        while (true) {
 
-        if (user.equals(comp)) {
-            System.out.println("There is a draw (" + comp + ")");
-        } else if (
-                (user.equals("rock") && comp.equals("scissors")) ||
-                        (user.equals("scissors") && comp.equals("paper")) ||
-                        (user.equals("paper") && comp.equals("rock"))
-        ) {
-            System.out.println("Well done. The computer chose " + comp + " and failed");
-        } else {
-            System.out.println("Sorry, but the computer chose " + comp);
+            String user = scanner.nextLine();
+
+            if (user.equals("!exit")) {
+                System.out.println("Bye!");
+                break;
+            }
+
+            if (!user.equals("rock") && !user.equals("paper") && !user.equals("scissors")) {
+                System.out.println("Invalid input");
+                continue;
+            }
+
+            String comp = options[random.nextInt(3)];
+
+            if (user.equals(comp)) {
+                System.out.println("There is a draw (" + comp + ")");
+            }
+            else if (
+                    (user.equals("rock") && comp.equals("scissors")) ||
+                            (user.equals("scissors") && comp.equals("paper")) ||
+                            (user.equals("paper") && comp.equals("rock"))
+            ) {
+                System.out.println("Well done. The computer chose " + comp + " and failed");
+            }
+            else {
+                System.out.println("Sorry, but the computer chose " + comp);
+            }
         }
     }
 }

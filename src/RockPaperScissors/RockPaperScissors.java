66e3094f98b806aa19b78dfc5746b865c5d606ae
oyyasumi177 <o.i.ivchenko@student.khai.edu>
2025-12-07 13:03:48 +0200

    RPS srage 2 complete

--- src/RockPaperScissors/RockPaperScissors.java
+++ src/RockPaperScissors/RockPaperScissors.java
@@ -1,27 +1,29 @@
 package RockPaperScissors;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class RockPaperScissors {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        String user = scanner.nextLine();   
-        String comp = "";
+        String user = scanner.nextLine();
 
-        switch (user) {
-            case "rock":
-                comp = "paper";
-                break;
-            case "paper":
-                comp = "scissors";
-                break;
-            case "scissors":
-                comp = "rock";
-                break;
+        String[] options = {"rock", "paper", "scissors"};
+
+        Random random = new Random();
+        String comp = options[random.nextInt(3)];
+
+        if (user.equals(comp)) {
+            System.out.println("There is a draw (" + comp + ")");
+        } else if (
+                (user.equals("rock") && comp.equals("scissors")) ||
+                        (user.equals("scissors") && comp.equals("paper")) ||
+                        (user.equals("paper") && comp.equals("rock"))
+        ) {
+            System.out.println("Well done. The computer chose " + comp + " and failed");
+        } else {
+            System.out.println("Sorry, but the computer chose " + comp);
         }
-
-        System.out.println("Sorry, but the computer chose " + comp);
     }
 }
-
